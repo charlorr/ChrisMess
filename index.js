ae381e06629b214dd8b6d2caa00f33b52ed01e4f
Charlene Orr <charlorrnot@gmail.com>
2018-06-25 14:14:56 -0400

    Add button functionality

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+function changeText() {
+    const text = document.querySelector('#second');
+    text.textContent = 'They\'re the season for the reason.';
+}
+
 const button = document.querySelector('button');
+
+button.addEventListener('click', changeText);
